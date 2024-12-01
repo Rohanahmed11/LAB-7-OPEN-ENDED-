767465763f00d6124fe0328d099dcf392d841114
fff <fff@DESKTOP-6HOQQJ6>
2024-12-01 20:07:18 +0000

    Initial Commit

---
+++ GuessNumber/src/Task2/GuessNumber.java
@@ -0,0 +1,20 @@
+package Task2;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        int numberToGuess = 1; 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Guess the number (1-10): ");
+        int userGuess = scanner.nextInt();
+
+        if (userGuess == numberToGuess) {
+            System.out.println("Congratulations! You guessed it right!");
+        } else {
+            System.out.println("Sorry, the correct number was " + numberToGuess);
+        }
+        
+        scanner.close();
+    }
+}
